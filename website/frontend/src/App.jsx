cc18a129abdf55d97f7c69b7bcf13569cb14f458
Febin Evijin <15febinevijin@gmail.com>
2022-12-16 12:01:13 +0530

    server created

---
+++ website/frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">ecommerse</Link>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
