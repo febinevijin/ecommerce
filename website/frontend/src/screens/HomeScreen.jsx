e034f449bb425a52bdb55632afc8279a99038497
Febin Evijin <15febinevijin@gmail.com>
2022-12-19 11:01:16 +0530

    new commit

--- website/frontend/src/screens/HomeScreen.jsx
+++ website/frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import axios from "axios";
+import logger from 'use-reducer-logger'
+// import data from "../data";
+
+const reducer = (state, action) => {
+  switch (action.type) { 
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCESS':
+      return { ...state, products: action.payload, loading: false };
+     case 'FETCH_FAIL':
+      return { ...state,loading: false, error: action.payload };
+    default:
+      return state;
+    
+  }
+}
 
 function HomeScreen() {
+  // const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products:[],
+    loading: true,
+    error: " ",
+  })
+  useEffect(() => {
+    const fetchData = async () => {
+
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCESS", payload: result.data });
+      } catch (err){
+        dispatch({ type: "FETCH_FAIL", payload: err.message })
+      }
+      // setProducts(result.data)
+    } 
+    fetchData();
+  }, [])
+  
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {loading ? 
+          (<div>loading...</div>) :
+          // error ?
+          //   (<div>jhfghfhghgf</div>) :
+          (
+          products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
@@ -22,7 +64,7 @@ function HomeScreen() {
               <button>Add to cart</button>
             </div>
           </div>
-        ))}
+        )))}
       </div>
     </div>
   );
