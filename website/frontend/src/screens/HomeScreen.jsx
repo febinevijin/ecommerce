0175ac87c833fa4c6838749037ec82c77b6a2d78
Febin Evijin <15febinevijin@gmail.com>
2022-12-19 13:24:55 +0530

    frontend of cardSection-review,rating,cardDesgn

--- website/frontend/src/screens/HomeScreen.jsx
+++ website/frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useReducer, useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import logger from 'use-reducer-logger'
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
 // import data from "../data";
 
 const reducer = (state, action) => {
@@ -23,7 +24,7 @@ function HomeScreen() {
   const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
     products:[],
     loading: true,
-    error: " ",
+    error: "",
   })
   useEffect(() => {
     const fetchData = async () => {
@@ -44,27 +45,19 @@ function HomeScreen() {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {loading ? 
-          (<div>loading...</div>) :
-          // error ?
-          //   (<div>jhfghfhghgf</div>) :
-          (
-          products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          </div>
-        )))}
+        {loading ? (
+          <div>loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
       </div>
     </div>
   );
