7d9591c5a916edaf9baceead09716bb964ffa0e2
Febin Evijin <15febinevijin@gmail.com>
2022-12-22 11:04:53 +0530

    complete add to cart functionalities

--- website/frontend/src/screens/ProductScreen.jsx
+++ website/frontend/src/screens/ProductScreen.jsx
@@ -44,10 +44,21 @@ function ProductScreen() {
   }, [slug]);
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const addToCartHandler = () => {
+  const {cart} = state;
+  const addToCartHandler = async() => {
+    const existItem = cart.cartItems.find(x => x._id === product._id);
+     
+    const quantity = existItem ? existItem.quantity + 1 : 1;      
+  
+
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      alert("Sorry, the product is out of stock");
+      return;
+    }
     ctxDispatch({
       type: "CART_ADD_ITEM",
-      payload: { ...product, quantity: 1 },
+      payload: { ...product, quantity },
     });
   };
   return loading ? (
