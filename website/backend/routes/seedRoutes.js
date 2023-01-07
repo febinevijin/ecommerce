44df96d178d52454dca0fcf2c7e0e6644c0979a2
Febin Evijin <15febinevijin@gmail.com>
2023-01-07 11:17:30 +0530

    Seed sample users

--- website/backend/routes/seedRoutes.js
+++ website/backend/routes/seedRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import data from "../data.js";
-import Product from "../models/productModels.js";
+import Product from "../models/productModel.js";
+import User from "../models/userModel.js";
 
 
 const seedRouter = express.Router();
@@ -8,7 +9,9 @@ const seedRouter = express.Router();
 seedRouter.get('/', async (req,res) => {
     await Product.remove({});
     const createdProducts = await Product.insertMany(data.products);
-    res.send({ createdProducts });
+    await User.remove({});
+    const createdusers = await User.insertMany(data.users);
+    res.send({createdProducts, createdusers });
 })
 
 export default seedRouter;
