7b923c40f281df557c67a9c51c4da4b3ecbc77d2
Febin Evijin <15febinevijin@gmail.com>
2023-01-08 22:44:44 +0530

    create signin backend api

--- website/backend/routes/seedRoutes.js
+++ website/backend/routes/seedRoutes.js
@@ -7,11 +7,11 @@ import User from "../models/userModel.js";
 const seedRouter = express.Router();
  
 seedRouter.get('/', async (req,res) => {
-    await Product.remove({});
-    const createdProducts = await Product.insertMany(data.products);
-    await User.remove({});
-    const createdusers = await User.insertMany(data.users);
-    res.send({createdProducts, createdusers });
+    // await Product.remove({});
+    // const createdProducts = await Product.insertMany(data.products);
+    // await User.remove({});
+    // const createdusers = await User.insertMany(data.users);
+    // res.send({createdProducts, createdusers });
 })
 
 export default seedRouter;
